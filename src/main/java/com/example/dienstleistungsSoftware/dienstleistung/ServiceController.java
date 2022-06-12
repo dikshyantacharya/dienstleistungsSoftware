48d046c11b29befcbae40b29f61c4c7d717952ee
NOVOMIND\dacharya <Shrey@dhung@n@123>
2022-06-12 12:31:01 +0200

    cors issue fixed

--- src/main/java/com/example/dienstleistungsSoftware/dienstleistung/ServiceController.java
+++ src/main/java/com/example/dienstleistungsSoftware/dienstleistung/ServiceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://dienstleistungs-software.herokuapp.com/", allowedHeaders = "*")
 @RestController
 @RequestMapping("/service")
 @Service
--- src/main/java/com/example/dienstleistungsSoftware/user/UserController.java
+++ src/main/java/com/example/dienstleistungsSoftware/user/UserController.java
@@ -3,7 +3,7 @@ package com.example.dienstleistungsSoftware.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://dienstleistungs-software.herokuapp.com/", allowedHeaders = "*")
 @RequestMapping("/user")
 @RestController
 public class UserController {
